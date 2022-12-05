2ff7043370a76266044351ab91f7ae212b5e178f
Julian Canepa <julian.l.canepa@gmail.com>
2022-12-04 22:50:30 -0800

    Translate percieved intention of main code block by intuition.

--- main.rb
+++ main.rb
@@ -8,9 +8,11 @@ require_relative 'src/world'
 
 
 def main()
+  # two types of waypoints?
   w = Waypoint.new(-121.5, 45.5, 30, "home", "flag")
   w2 = Waypoint.new(-121.5, 45.6, nil, "store", "dot")
 
+  # three collections of points
   ts1 = [
     Point.new(-122, 45),
     Point.new(-122, 46),
@@ -25,11 +27,14 @@ def main()
     Point.new(-122, 45.5),
   ]
 
+  # new tracks are instantiated with a collection of points, and a name
   t = Track.new([ts1, ts2], "track 1")
   t2 = Track.new([ts3], "track 2")
 
+  # create world with name and a collection of (waypoints and tracks)
   world = World.new("My Data", [w, w2, t, t2])
 
+  # echo a json representation of the world
   puts world.to_geojson()
 end
 
